6bc137c3970011fd8096d920b4efeb97e19540ab
taitd <tai.td@citigo.net>
2022-10-21 16:36:42 +0700

    MetaMask TestNet alchemy.com goerli

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,19 @@
-require("@nomicfoundation/hardhat-toolbox");
+// require("@nomicfoundation/hardhat-toolbox");
 
-/** @type import('hardhat/config').HardhatUserConfig */
+// /** @type import('hardhat/config').HardhatUserConfig */
+// module.exports = {
+//   solidity: "0.8.17",
+// };
+
+
+require('@nomiclabs/hardhat-waffle')
+
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: 'https://eth-mainnet.g.alchemy.com/v2/g-Yes-x8NdMoaxC_qE5ymZIjSfz_01xr',
+      accounts: ['58ecd49400427f57ecfeac42a032dcdc99c10d9fd27c47902ed7d3bb33dc1df6']
+    }
+  }
 };
