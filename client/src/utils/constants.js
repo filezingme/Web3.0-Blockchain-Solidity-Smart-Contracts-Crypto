12ff7f9263e1569da87d39fc42562c2ca42fce9a
taitd <tai.td@citigo.net>
2022-10-24 16:36:23 +0700

    Blockchain, connectWallet, sendTransaction, TransactionContext.Provider

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json'
+
+export const contractABI = abi.abi
+export const contractAddress = '0xAdF02BA4B99AD8124961Be59F96c26fD99578326'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -7,12 +7,19 @@
 
 
 require('@nomiclabs/hardhat-waffle')
+//require('@nomicfoundation/hardhat-toolbox')
+//require("@nomicfoundation/hardhat-chai-matchers")
+
+//require('dotenv').config()
+
+// const GOERLI_RPC_URL = process.env.RPC_URL
+// const PRIVATE_KEY = process.env.PRIVATE_KEY
 
 module.exports = {
   solidity: "0.8.17",
   networks: {
     goerli: {
-      url: 'https://eth-mainnet.g.alchemy.com/v2/g-Yes-x8NdMoaxC_qE5ymZIjSfz_01xr',
+      url: 'https://eth-goerli.g.alchemy.com/v2/rtTVQWc_rgwDm4mkaYi-k0_OtHWm4gsI',
       accounts: ['58ecd49400427f57ecfeac42a032dcdc99c10d9fd27c47902ed7d3bb33dc1df6']
     }
   }
